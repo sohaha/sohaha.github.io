4bdef00105b6f3aad76fd1177adebe8cc5f4f8ac
arryuu <604125002@qq.com>
2021-05-06 10:02:06 +0800

    docs: 补充编辑器智能提示;

--- vue-admin-template/typings/index.d.ts
+++ vue-admin-template/typings/index.d.ts
@@ -10,22 +10,106 @@ declare class d_vue {
   nextTick(callback: Function)
 }
 
-declare class d_util {
-  useInitTitle(ctx)
+declare class useTip {
+  message  (type: string, tip: string, duration: number): void
+  notify (type: string, tip: string, title = '温馨提示', duration: number): void
+}
 
+declare class useConfirm {
+  warning (title: string, message: string, confirmFn: Function, cancelFn: Function, opt: object): any
+}
+
+declare class useInitTitle {
+  title
+  SpaTitle
+}
+
+declare class d_util {
+  useTip(): useTip
+  useConfirm(): useConfirm
+  useInitTitle(ctx): useInitTitle
   formCreate()
 }
 
-declare class d_hook {
-  useTip()
+declare class useRouter {
+  go (index)
+  replace (path, query)
+  push (path, query)
+  getQuery ()
+  children ()
+  resetRouter (routerData = [], global = false)
+}
 
-  useRouter(ctx)
+declare class useStore {
+  state: any
+  commit()
+  getters: any
+  dispatch()
+}
+
+declare class useCache {
+  getCache (key, def)
+  setCache (key, value, expi)
+}
+
+declare class useLoading {
+  loading
+  setLoading (value)
+  withLoading (task, {autocomplete = true} = {})
+}
+
+declare class d_hook {
+  useRouter(ctx): useRouter
+  useStore(ctx, module): useStore
+  useCache(option, storage): useCache
+  useLoading(): useLoading
+  useHttp()
+  useWebWorker()
+  useWindowSize()
+  useWindowSizeRealTime()
 }
 
 declare class d_app {
+  vm: object
+  request: object
+  router: object
+  store: object
+  userData: object
+  defaultAvatar: string
+  hasPermission(key)
+  setVm(app)
+  initStore()
+  resetRouter(router)
+  initRouter()
+  requestInit()
+}
+
+declare class useRequestPage {
+  change()
+  search(key)
+  key: string
+  items: array
+  pages: object
+  reload
+  error
+  data
+  loading
+  run
+}
+
+declare class useRequest {
+  loading
+  withLoading
+  data
+  error
+  cancel
+  run
 }
 
 declare class d_apiUtil {
+  useRequestPage(fn, page, o): useRequestPage
+  useRequestWith(fn, o = {})
+  useRequest(fn, o = {}): useRequest
 }
 
 
